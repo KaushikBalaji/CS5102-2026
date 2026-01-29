a8a0c8fde1192f70b9532811dd718d4bb54d7d27
KaushikBalaji <kaushikbalaji0609@outlook.com>
2026-01-29 21:05:50 +0530

    Report images and Codes

---
+++ Ex1/CPP files/FIR_ALL.cpp
@@ -0,0 +1,13 @@
+
+#define N 11
+#include <ap_int.h>
+extern void FIR1(int *,int);
+extern void FIR2(int *,int);
+extern void FIR3(int *,int);
+extern void FIR4(int *,int);
+void FIR_ALL(int *y, int x){
+  //FIR1(y,x);
+  //FIR2(y,x);
+  //FIR3(y,x);
+  FIR4(y,x);
+}
